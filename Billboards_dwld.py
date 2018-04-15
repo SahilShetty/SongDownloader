5071a780c4eb67c1974bc7b45e62dea212328b40
SahilShetty <35658006+SahilShetty@users.noreply.github.com>
2018-04-15 21:06:39 +0530

    Update Billboards_dwld.py

--- Billboards_dwld.py
+++ Billboards_dwld.py
@@ -1,37 +1,66 @@
 import moviepy.editor as mp
 from requests import get
 import os
-from string import ascii_letters as al
+from string import *
 from bs4 import BeautifulSoup as Bsoup
 from pytube import YouTube
 
 
 def main(names = [], bsoup1 = Bsoup(get('https://www.billboard.com/charts/hot-100').text, 'lxml'), titles2 = [], remove = [], count = 0):
 
-    def loop1(name):
+    def searchUrl(name):
 
         for i in range(len(name) - 1):
 
             if name[i] == ' ': name[i] = '+'
             
-            if name[i] not in al and name[i] != '+':
+            if name[i] not in ascii_letters and name[i] != '+':
                 
                 del name[i]
-                loop1(name)
+                searchUrl(name)
 
         return name
 
 
-    path1 = raw_input('Path of any folder: ')
+    def video(bsoup2):
+
+        for lnk in bsoup2.findAll('a'):
+
+            href = lnk.get('href')
+            
+            if '/watch?v=' in href:
+
+                for vName in bsoup2.findAll('a', {'href': href}):
+
+                    if vName.get('title') != None:
+
+                        titles1 = list(vName.get('title'))
+
+                        for letter in titles1:
+
+                            if letter not in tuple(ascii_letters) + tuple(punctuation) + tuple(' '):
+
+                                truth = False
+                                break
+                            
+                            else: truth = True
+
+                        if truth:
+
+                            url = ('https://www.youtube.com' + href)
+                            return (url, titles1)
+
+
+    path1 = 'C:\Users\Seema\Desktop\Songs' # raw_input('Path of any folder: ')
     os.chdir(path1)
-    
+
     try:
         
         os.mkdir('mp4')
         os.mkdir('mp3')
 
     except WindowsError: None
-    
+
     amount = raw_input('Range of songs\nformat - starting song + colon and space + ending song\nexample n1: n2: ').split(': ')
     while (1 + int(amount[1]) - int(amount[0])) > 11:
         amount = raw_input('You can only download 10 songs at a time before getting dreops in quality (change ip after re-doing): ').split(': ')
@@ -46,41 +75,27 @@ def main(names = [], bsoup1 = Bsoup(get('https://www.billboard.com/charts/hot-10
             
         else: continue
 
-    for name in names:
+    for name_ in names:
 
-        name_ = name
         print '\n' + name_, 'is downloading'
         
-        name = list(name)
-        name = loop1(name)
-        if name[-1] not in al: del name[-1]
+        name = searchUrl(list(name_))
+        if name[-1] not in ascii_letters: del name[-1]
         name = ''.join(name)
 
         bsoup2 = Bsoup(get('https://www.youtube.com/results?search_query=' + name + '+lyrics').text, 'lxml')
+        (url, titles1) = video(bsoup2)
         
-        for lnk in bsoup2.findAll('a'):
-
-            href = lnk.get('href')
-            
-            if '/watch?v=' in href:
-                
-                url = ('https://www.youtube.com' + href)
-                break
-
-        for vName in bsoup2.findAll('a', {'href': href}): titles = vName.get('title')
-        titles1 = list(titles)
-
         for t in titles1:
 
-            if t == '(' or t == ')' or t == ' ' or t == '-' or t == '&' or t in al: titles2.append(t)
+            if t == '(' or t == ')' or t == ' ' or t == '-' or t == '&' or t in ascii_letters: titles2.append(t)
 
         try: yt = YouTube(url)
         
         except:
             
             print 'Sorry, but those are all the songs I can download not.'
-            print 'Please click "okay"'
-            exit()
+            raise KeyboardInterrupt
         
         stream = yt.streams.first()
         stream.download(path1 + '/mp4')
@@ -95,10 +110,10 @@ def main(names = [], bsoup1 = Bsoup(get('https://www.billboard.com/charts/hot-10
             clip.audio.write_audiofile(path1 + '/mp3/' + name_ + ".mp3")
 
         except IOError: print 'Cannot download', "'" + name_ + "'"
-        
+
         titles2 = []
+        print "'" + name_ + "'", 'has downloaded'
 
-    return 'COMPLETE'
 
-print 'Use of VPN is manditory'
+print 'For over 7 song downloads, VPN is needed'
 print main()
