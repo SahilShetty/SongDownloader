94a6e84156f3e2d51e3a9eb4619b1f3ec78c7d32
Dr_Claymore <35658006+SahilShetty@users.noreply.github.com>
2018-04-22 00:27:09 +0530

    Update Billboards_dwld.py

--- Billboards_dwld.py
+++ Billboards_dwld.py
@@ -6,17 +6,18 @@ from bs4 import BeautifulSoup as Bsoup
 from pytube import YouTube
 
 
-def main(path1, amount, names = [], bsoup1 = Bsoup(get('https://www.billboard.com/charts/hot-100').text, 'lxml'), titles2 = [], remove = [], error = False):
+def main(path1, pre_dwded, amount = raw_input('Range of songs: ').split(': '), count = 0, names = [], bsoup1 = Bsoup(get('https://www.billboard.com/charts/hot-100').text, 'lxml'), titles2 = [],
+         remove = [], error = False):
 
     def searchUrl(name):
 
-        for i in range(len(name) - 1):
+        for lett in range(len(name) - 1):
 
-            if name[i] == ' ': name[i] = '+'
+            if name[lett] == ' ': name[lett] = '+'
             
-            if name[i] not in ascii_letters and name[i] != '+':
+            if name[lett] not in ascii_letters and name[lett] != '+':
                 
-                del name[i]
+                del name[lett]
                 searchUrl(name)
 
         return name
@@ -60,25 +61,15 @@ def main(path1, amount, names = [], bsoup1 = Bsoup(get('https://www.billboard.co
 
     except WindowsError: None
 
-    if amount == None:
-        
-        amount = raw_input('Range of songs\nformat - starting song + colon and space + ending song\nexample n1: n2: ').split(': ')
-        
-        while int(amount[0]) >= int(amount[1]) and int(amount[1]) >= 100: amount = raw_input('Enter valid values: ').split(': ')
-
-    count = int(amount[0]) - 1
-
-    for i in bsoup1.findAll('h2', {'class': 'chart-row__song'}):
+    for h2 in bsoup1.findAll('h2', {'class': 'chart-row__song'}):
 
         count += 1
         
-        if count >= int(amount[0]) and count <= int(amount[1]): names.append(str(i.string))
-            
-        else: continue
+        if count >= int(amount[0]) and count <= int(amount[1]) and str(h2.string) + '\n' not in pre_dwded: names.append(str(h2.string))
 
     for name_ in names:
 
-        print '\n' + "'"+ name_ + "'", 'is downloading'
+        print '\n' + "'" + name_ + "'", 'is downloading'
         
         name = searchUrl(list(name_))
         if name[-1] not in ascii_letters: del name[-1]
@@ -95,8 +86,8 @@ def main(path1, amount, names = [], bsoup1 = Bsoup(get('https://www.billboard.co
         
         except:
             
-            print 'Sorry, but those are all the songs I can download not.'
-            raise KeyboardInterrupt
+            print 'Restart'
+            return []
         
         stream = yt.streams.first()
         
@@ -104,8 +95,8 @@ def main(path1, amount, names = [], bsoup1 = Bsoup(get('https://www.billboard.co
         
         except:
 
-            print 'Restarting'
-            main(path1, [str(count), amount[1]])
+            print 'Restart'
+            return []
 
         titles2 = ''.join(titles2)
         path2 = path1 + '\\mp4\\' + titles2
@@ -120,9 +111,34 @@ def main(path1, amount, names = [], bsoup1 = Bsoup(get('https://www.billboard.co
 
         titles2 = []
         if not error: print "'" + name_ + "'", 'has downloaded'
-        else: print 'Cannot download', "'" + name_ + "'"
+        else:
+
+            names.remove(name_)
+            print 'Cannot download', "'" + name_ + "'"
         error = False
 
+    return names
 
-main(raw_input('Path: '), None)
+
+path = raw_input('Path: ')
+
+openfil = lambda mode, path: open(path + '\\dwd.txt', mode)
+
+try:
+
+    fil = openfil('r', path)
+    pre_dwded = fil.readlines()
+    
+
+except IOError:
+    
+    fil = openfil('w', path)
+    pre_dwded = []
+
+fil.close()
+
+fil = openfil('a', path)
+for already in main(path, pre_dwded): fil.write(already + '\n')
+fil.close()
+
 print 'FINISHED'
